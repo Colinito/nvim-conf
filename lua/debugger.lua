e8d6e2d516d1c5e68729da2f41c2875b28e1dba9
Colin Barnum <colinmbarnum@gmail.com>
2024-07-17 22:12:47 -0400

    Combine launch.json with other DAP configurations for Go

--- lua/debugger.lua
+++ lua/debugger.lua
@@ -1,4 +1,3 @@
-require('dap.ext.vscode').load_launchjs('launch.json', {})
 
 local dap = require('dap')
 
@@ -11,6 +10,37 @@ dap.adapters.go = {
 	},
 }
 
+dap.configurations.go = {
+	{
+		type = "go",
+		name = "Debug",
+		request = "launch",
+		program = "${file}",
+	},
+	{
+		type = "go",
+		name = "Debug Package",
+		request = "launch",
+		program = "${fileDirname}",
+	},
+	{
+		type = "go",
+		name = "Debug test",
+		request = "launch",
+		mode = "test",
+		program = "${file}",
+	},
+	{
+		type = "go",
+		name = "Debug test (go.mod)",
+		request = "launch",
+		mode = "test",
+		program = "./${relativeFileDirname}",
+	},
+}
+
+require('dap.ext.vscode').load_launchjs('./launch.json', {})
+
 vim.fn.sign_define('DapBreakpoint', {text = '🅱️', texthl = '', linehl = '', numhl = ''})
 
 local dapui = require('dapui')
@@ -21,5 +51,6 @@ vim.keymap.set('n', '<leader>dn', dap.step_over, {})
 vim.keymap.set('n', '<leader>ds', dap.step_into, {})
 vim.keymap.set('n', '<leader>do', dap.step_out, {})
 vim.keymap.set('n', '<leader>dt', dap.terminate, {})
+vim.keymap.set('n', '<leader>dr', dap.restart, {})
 vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint, {})
 vim.keymap.set('n', '<leader>du', dapui.toggle, {})
