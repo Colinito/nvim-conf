3a8abb975c83dfc8589901372adc7a270fc006f7
Colin Barnum <colinmbarnum@gmail.com>
2025-04-14 20:30:48 -0400

    Replace telescope, neotree, and toggleterm with snacks

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,34 +1,16 @@
 return {
-	"ellisonleao/gruvbox.nvim",
-	priority = 1000 ,
-	config = function()
-		vim.o.background = "dark" -- or "light" for light mode
-		vim.cmd[[colorscheme gruvbox]]
-
-		return {
-			terminal_colors = true, -- add neovim terminal colors
-			undercurl = true,
-			underline = true,
-			bold = true,
-			italic = {
-				strings = true,
-				emphasis = true,
-				comments = true,
-				operators = false,
-				folds = true,
-			},
-			strikethrough = true,
-			invert_selection = false,
-			invert_signs = false,
-			invert_tabline = false,
-			invert_intend_guides = false,
-			inverse = true, -- invert background for search, diffs, statuslines and errors
-			contrast = "", -- can be "hard", "soft" or empty string
-			palette_overrides = {},
-			overrides = {},
-			dim_inactive = false,
-			transparent_mode = false,
-		}
-	end,
-	opts = {},
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			vim.cmd[[colorscheme tokyonight]]
+		end
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000 ,
+		config = function()
+			-- vim.o.background = "dark" -- or "light" for light mode
+			-- vim.cmd[[colorscheme gruvbox]]
+		end
+	},
 }
