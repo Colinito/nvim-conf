82380e3151ed68f8d1cd31b2ad896c8fac31defd
Colin Barnum <colinmbarnum@gmail.com>
2025-11-18 13:34:28 -0500

    Bring back nvim-lspconfig for all its configurations

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -2,6 +2,29 @@ return {
 	{
 		"ellisonleao/gruvbox.nvim",
 		config = function()
+			-- require("gruvbox").setup({
+			-- 	terminal_colors = true,
+			-- 	undercurl = true,
+			-- 	underline = true,
+			-- 	bold = false,
+			-- 	italic = {
+			-- 		strings = false,
+			-- 		emphasis = false,
+			-- 		comments = false,
+			-- 		operators = false,
+			-- 		folds = false,
+			-- 	},
+			-- 	strikethrough = true,
+			-- 	invert_selection = false,
+			-- 	invert_signs = false,
+			-- 	invert_tabline = false,
+			-- 	inverse = true, -- invert background for search, diffs, statuslines and errors
+			-- 	contrast = "hard", -- can be "hard", "soft" or empty string
+			-- 	palette_overrides = {},
+			-- 	overrides = {},
+			-- 	dim_inactive = false,
+			-- 	transparent_mode = false,
+			-- })
 			vim.cmd[[colorscheme gruvbox]]
 		end
 	},
